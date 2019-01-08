79988257c60491df8f8626d8f7852190549e855e
Alexis Foerster <alexis.foerster@gmail.com>
2019-01-08 20:15:00 +0100

    Implémentation du module Traitements.

---
+++ Traitements.py
@@ -0,0 +1,34 @@
+# coding: utf-8
+
+# ==============================================================================
+# Name        : Traitements.py
+# Author      : Alexis Foerster (alexis.foerster@gmail.com)
+# Version     : 1.5 (08/01/2019)
+# Description : Traitements des récupérations
+# ==============================================================================
+
+if __name__ == "__main__":
+
+    from RecuperationPersonnages import recupererPersonnages
+    recupererPersonnages("9233364398528028107", "ListePersonnages.csv")
+
+    from RecuperationProfils import recupererProfils
+    recupererProfils("ListePersonnages.csv", "ListeProfils.csv")
+
+    from RecuperationRecettes import recupererRecettes
+    recupererRecettes("ListeRecettes.csv")
+
+    from RecuperationMateriauxCristaux import recupererMateriauxCristaux
+    recupererMateriauxCristaux("ListeRecettes.csv", "ListeMateriauxCristaux.csv")
+
+    from Personnages import construireListePersonnages
+    listePersonnages = construireListePersonnages("ListePersonnages.csv")
+    for nom, personnage in sorted(listePersonnages.personnages.iteritems()):
+        print "==================== %s ====================" % (nom)
+        print "%s" % (personnage.getTexteRiche())
+
+    from Recettes import construireListeRecettes
+    listeRecettes = construireListeRecettes("ListeRecettes.csv")
+    for nom, recette in sorted(listeRecettes.recettes.iteritems()):
+        print "==================== %s ====================" % (nom)
+        print "%s" % (recette.getTexteRiche())
